6817ca1ff86195cb513779ad37d5e298787f2d36
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2022-03-28 16:19:28 -0300

    Atualização no Option

--- js.js
+++ js.js
@@ -1,4 +1,14 @@
-var select = document.querySelector('select');
-select.addEventListener('Option1', function() { 
-    window.alert("Tudo okay")
-})
+
+
+
+
+function menus(valor) { 
+    if(valor == "index") { 
+        window.location.href = "index.html"
+    }
+    else if(valor == "1") { 
+        window.location.href = "indexRegistro.html"
+    } else if(valor == "2") { 
+        window.location.href = "indexH.html"
+    }
+    }
