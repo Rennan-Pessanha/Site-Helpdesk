918c07b386318059d7d103a2db2501395075579a
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2022-03-30 18:27:57 -0300

    Responsividade adicionada com menu bar, e menu modificado

--- js.js
+++ js.js
@@ -1,14 +1,9 @@
+function burguer() {
+let menuMobile = document.querySelector('.mobile-menu')
+if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove("open")
+} else { 
+    menuMobile.classList.add('open')
+}
 
-
-
-
-function menus(valor) { 
-    if(valor == "index") { 
-        window.location.href = "index.html"
-    }
-    else if(valor == "1") { 
-        window.location.href = "indexRegistro.html"
-    } else if(valor == "2") { 
-        window.location.href = "indexH.html"
-    }
-    }
+}
