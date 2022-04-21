a99d392ba01ebf43ec214ee429069bce853f3d02
Matheus Davila <mat.97@hotmail.com>
2022-04-21 12:50:51 -0300

    Atualização de formulário

--- js.js
+++ js.js
@@ -27,3 +27,21 @@ for (var i = 0; i < conteudos.length; i++) {
 
 document.getElementById(idtab).style.display = 'block'
 }
+
+function dados(){
+    let dados_total = document.getElementById('dados_total').value;
+    id = dados_total.split('\t')[0];
+    cpf = dados_total.split('\t')[1];
+    nome = dados_total.split('\t')[2];
+
+    input_id = document.getElementById('id_id');
+    input_cpf = document.getElementById('id_cpf');
+    input_nome = document.getElementById('id_nome');
+
+    input_id.value = id;
+    input_cpf.value = cpf;
+    input_nome.value = nome;
+
+}
+
+document.getElementById('btn_dados').onclick = dados;
