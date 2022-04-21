403201be6f6ec573657420392ffd110627e768d1
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2022-04-21 12:20:21 -0300

    Estilização do campo de formulário

--- js.js
+++ js.js
@@ -13,3 +13,17 @@ if (menuMobile.classList.contains('open')) {
 }
 
 }
+
+
+document.getElementById('tabpadrao').click()
+
+function abrirtab(idtab) {
+
+var conteudos = document.getElementsByClassName('form-content')
+
+for (var i = 0; i < conteudos.length; i++) {
+    conteudos[i].style.display = 'none'
+}
+
+document.getElementById(idtab).style.display = 'block'
+}
