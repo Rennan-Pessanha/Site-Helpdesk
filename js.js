f694eb9fe1fbe541333aed4d703b79ebf0e50249
Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
2022-05-16 17:19:32 -0300

    Configuração do menu superior.

--- js.js
+++ js.js
@@ -322,6 +322,52 @@ function Clicar(valor) {
     }
 
 
+    // Dropdown-Menu-Superior.
+
+    var btn = document.querySelector("#Perfil");
+
+    btn.addEventListener("click", function() {
+    
+        var div = document.querySelector(".Sub-Menu-Perfil");
+        
+      if(div.style.display === "none") {
+            div.style.display = "block";
+        } else {
+          div.style.display = "none";
+      }
+        
+    });
+
+    var btn = document.querySelector("#Not");
+
+    btn.addEventListener("click", function() {
+    
+        var div = document.querySelector(".Sub-Menu-Not");
+        
+      if(div.style.display === "none") {
+            div.style.display = "block";
+        } else {
+          div.style.display = "none";
+      }
+        
+    });
+
+    var btn = document.querySelector("#Tarefas");
+
+    btn.addEventListener("click", function() {
+    
+        var div = document.querySelector(".Sub-Menu-Tar");
+        
+      if(div.style.display === "none") {
+            div.style.display = "block";
+        } else {
+          div.style.display = "none";
+      }
+        
+    });
+
+
+
 
 
 
