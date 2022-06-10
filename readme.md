2a59d97831edd22fedcaed2673903620886f427c
Matheus Davila <mat.97@hotmail.com>
2022-06-10 15:54:57 -0300

    att readme com comandos npm

--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@ Site para o setor Helpdesk (EM DESENVOLVIMENTO).
 Demonstração: https://rennan-pessanha.github.io/Site-Helpdesk/
 
 Roadmap do projeto: https://trello.com/invite/b/dREdR9Sh/edee62db05093a148bd12384c1c9c516/site-helpdesk
+
+
+Comandos utilizados:
+
+npm i express mongoose nodemon mongodb 
+
+npm run dev
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const path = require('path');
+const { send } = require('process');
 
 const routes = new Router();
 
@@ -7,6 +8,12 @@ routes.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
+routes.get('/atendimentos', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../views', 'Meus-Atendimentos.html'))
+})
 
+routes.get('/perfil', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../views', 'MeuPerfil.html'))
+})
 
 module.exports = routes;
