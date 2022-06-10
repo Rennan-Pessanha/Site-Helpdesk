d98322e5b3a9f86ea4479e0f6386671c7fd1f99e
Matheus Davila <mat.97@hotmail.com>
2022-06-10 15:01:29 -0300

    Correção diretórios index.html

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,8 +5,8 @@ const routes = new Router();
 
 routes.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'index.html'));
-    res.sendFile
 });
+
 
 
 module.exports = routes;
