d0f54a8311ee3efe21e682d113160e1140971411
WilliamProgramadorBR <william100william@gmail.com>
2022-12-12 23:50:18 -0300

    Motor de busca inserido, falta estilizar xD
    Eu comentei o css, estava afetando diretamente o código, depois é só refaze-lo.
    
    Co-Authored-By: Fabio Cupertino <fabiocupertino@yahoo.com.br>
    Co-Authored-By: Maria Jessica <107513875+MariaJessica404@users.noreply.github.com>
    Co-Authored-By: Rennan Pessanha <94031223+Rennan-Pessanha@users.noreply.github.com>
    Co-Authored-By: Gabriel Gouvea <103601293+gouvea77@users.noreply.github.com>
    Co-Authored-By: Marcelo Vinicius <111547992+marcelolcrespo@users.noreply.github.com>
    Co-Authored-By: marcusphillipe <67166960+marcusphillipe@users.noreply.github.com>
    Co-Authored-By: Matheus Davila <75381445+mdavilag@users.noreply.github.com>

--- src/public/Scripts/PadroesEmail.js
+++ src/public/Scripts/PadroesEmail.js
@@ -1,15 +1,65 @@
 
-function myFunction() {
-  let input = document.getElementById('myInput').value
-  input = input.toLowerCase();
-  let x = document.getElementsByClassName('ListUl');
+const inputPesquisa = document.querySelector("#myInput")
+const inputResult = document.querySelector("[data-type='nav-menu']")
+
+const details = Array.from(inputResult.querySelectorAll("details"))
+const lis = Array.from(inputResult.querySelectorAll("details li"))
+const lisToggle = Array.from(inputResult.querySelectorAll("li.toggle"))
+
+
+inputPesquisa.addEventListener('input',  ()=>{
     
-  for (i = 0; i < x.length; i++) { 
-      if (!x[i].innerHTML.toLowerCase().includes(input)) {
-          x[i].style.display="none";
-      }
-      else {
-          x[i].style.display="list-item";                 
-      }
-  }
+    const  srt = inputPesquisa.value;
+
+    if(srt){ 
+        
+        filterData(srt);
+        
+    } else{
+        
+    }
+    
+    
+})
+function allitens(){
+    lis.forEach(li => li.classList.remove("hide"))
+    details.forEach(detail => detail.removeAttribute("open"));
+
 }
+
+function filterData(srt){
+    
+    lisToggle.forEach(lisToggle =>{
+        const details = lisToggle.querySelector("details");
+        if(!details){
+            return;
+        }
+        
+        const summary =  details.querySelector("summary");
+ 
+        if(summary && summary.textContent.toLowerCase().includes(srt.toLowerCase)){
+            
+           return details.setAttribute("open", "")
+           
+        }
+        const lis = details.querySelectorAll("li")
+        
+        let found = false
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i]
+           
+            if (li.textContent.toLowerCase().includes(srt.toLowerCase())){
+                found = true
+                li.classList.remove("hide")
+                console.log(li.textContent)
+                
+            }else{
+                li.classList.add("hide")
+            }
+        }
+            
+        
+    })
+}
+
+
