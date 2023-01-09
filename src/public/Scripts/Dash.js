60a8f2f493fb22bdb4f793ff5991aebaa5143624
WilliamProgramadorBR <william100william@gmail.com>
2023-01-09 19:07:21 -0300

    Adicionado novas ideias

--- src/public/Scripts/Dash.js
+++ src/public/Scripts/Dash.js
@@ -1,5 +1,7 @@
 google.charts.load('current', {'packages':['annotationchart']});
 google.charts.load("current", {'packages':["corechart"]});
+google.charts.load("current", {packages:["timeline"]});
+      google.charts.setOnLoadCallback(ohbrabo);
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
@@ -54,6 +56,34 @@ google.charts.load("current", {'packages':["corechart"]});
 
         var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
         chart.draw(data, options);
+      };
+
+      
+      function ohbrabo() {
+        var container = document.getElementById('example5.4');
+        var chart = new google.visualization.Timeline(container);
+        var dataTable = new google.visualization.DataTable();
+        dataTable.addColumn({ type: 'string', id: 'Room' });
+        dataTable.addColumn({ type: 'string', id: 'Name' });
+        dataTable.addColumn({ type: 'date', id: 'Start' });
+        dataTable.addColumn({ type: 'date', id: 'End' });
+        dataTable.addRows([
+          [ 'Elias da costa gaes Assunção ',  'Estágiario',    new Date(0,0,0,12,0,0),  new Date(0,0,0,14,0,0) ],
+          [ 'Rennan Pessanha da Silva',  'Analista help desk',    new Date(0,0,0,14,30,0), new Date(0,0,0,16,0,0) ],
+          [ 'Maria Jessica Gomes de Souza',  'Analista help desk', new Date(0,0,0,16,30,0), new Date(0,0,0,19,0,0) ],
+          [ 'Leonardo Henrique de almeida', 'Atedente Help desk',   new Date(0,0,0,12,30,0), new Date(0,0,0,14,0,0) ],
+          [ 'João Gabriel Lima proença', 'Atendente Help desk',       new Date(0,0,0,14,30,0), new Date(0,0,0,16,0,0) ],
+          [ 'Felipe Satiro do Carmo de Souza', 'Atendente Help desk',        new Date(0,0,0,16,30,0), new Date(0,0,0,18,0,0) ],
+          [ 'Eduardo de Almeida Serpa ',   'Estágiario',       new Date(0,0,0,12,30,0), new Date(0,0,0,14,0,0) ],
+          [ 'Carlos Eduardo Farias Rocha ',   'Atendente Help desk',             new Date(0,0,0,14,30,0), new Date(0,0,0,16,0,0) ],
+          [ 'Juan Nascimento Mesquita do  Rosario ',   'Atendente Help desk',          new Date(0,0,0,16,30,0), new Date(0,0,0,18,30,0) ]]);
+    
+        var options = {
+          timeline: { colorByRowLabel: true },
+          alternatingRowStyle: false
+        };
+    
+        chart.draw(dataTable, options);
       }
     
 
