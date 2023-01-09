1098f5fc085980c31196a3c9ef668a32d5c2200b
WilliamProgramadorBR <william100william@gmail.com>
2023-01-09 18:29:46 -0300

    Dashboard falta estilizar algumas coisas

--- src/public/Scripts/Dash.js
+++ src/public/Scripts/Dash.js
@@ -1,4 +1,5 @@
 google.charts.load('current', {'packages':['annotationchart']});
+google.charts.load("current", {'packages':["corechart"]});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
@@ -11,17 +12,17 @@ google.charts.load('current', {'packages':['annotationchart']});
         data.addColumn('string', 'Gliese title');
         data.addColumn('string', 'Gliese text');
         data.addRows([
-          [new Date(1000, 2, 30), 7000, 'Inadimplência', 'undefined',
+          [new Date(15, 2, 30), 1000, 'Inadimplência', 'undefined',
                                   400, 'Bug de inadimplência', 'Bloqueio'],
-          [new Date(2000, 2, 16), 700, 'Login code', 'Liberação ou trocar', 
+          [new Date(30, 2, 16), 700, 'Login code', 'Liberação ou trocar', 
                                   , 'Bug de liberação de secundário', 'Bug Troca de principal'],
-          [new Date(3000, 2, 17), 700, 'Dúvidas academicas', 'They are very tall',
-                                  555, 'Sugestões', 'First Encounter'],
-          [new Date(4000, 2, 18), 800, 'Conflito de dados', 'Attack on our crew!',
+          [new Date(45, 2, 17), 700, 'Dúvidas academicas', 'aulas bônus',
+                                  555, 'Sugestões', 'sugestões e pontos de vistas'],
+          [new Date(70, 2, 18), 800, 'Conflito de dados', 'Attack on our crew!',
                                   555, 'Medsoft', 'Bugs não sondados'],
-          [new Date(5000, 2, 19), 555, 'Area restrita', 'Heavy casualties',
+          [new Date(95, 2, 19), 555, 'Area restrita', 'Heavy casualties',
                                   200, 'Concursos', 'Problemas'],
-          [new Date(6000, 2, 20), 400, 'Medeletro', 'Problemas de login',
+          [new Date(120, 2, 20), 400, 'Medeletro', 'Problemas de login',
                                   200, 'Plantão de recursos', 'bug plantão de recursos']
         ]);
 
@@ -32,4 +33,29 @@ google.charts.load('current', {'packages':['annotationchart']});
         };
 
         chart.draw(data, options);
+      };
+
+      
+      google.charts.setOnLoadCallback(putão);
+      function putão() {
+        var data = google.visualization.arrayToDataTable([
+          ['Login code', 'Problemas do medsoft'],
+          ['Login code',     11],
+          ['Plantão de recursos',      2],
+          ['Dúvidas acadêmicas',  2],
+          ['Login code', 2],
+          ['Limpeza de dados',    7]
+        ]);
+
+        var options = {
+          title: 'Medsoft',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
       }
+    
+
+
+      
