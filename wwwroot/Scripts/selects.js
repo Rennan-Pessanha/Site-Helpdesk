2077f6847559fe5a9f1c0095c25e3a89f2926a31
WilliamProgramadorBR <william100william@gmail.com>
2023-02-16 19:31:53 -0300

    Update selects.js

--- wwwroot/Scripts/selects.js
+++ wwwroot/Scripts/selects.js
@@ -1,9 +1,9 @@
-var xhttp = new XMLHttpRequest();
+let xhttp = new XMLHttpRequest();
 xhttp.open("GET", 'https://localhost:7190/listacategorias', false);
 xhttp.send();
-var categorias = xhttp.responseText;
+let categorias = xhttp.responseText;
 categorias = JSON.parse(categorias)  
- 
+
 let lista = Array(categorias)
 console.log (lista)
 lista.forEach(e => {
@@ -19,20 +19,24 @@ lista.forEach(e => {
  
     e.aplicacao.forEach(e => {
         select1.options[select1.options.length] = new Option(e.valor, e.id) 
-        console.log(e.valor)
+        console.log(e)
         });
+       
 
     e.area.forEach(e => {
         select2.options[select2.options.length] = new Option(e.valor, e.id) 
-        console.log(e.valor)
+        console.log(e)
         });
 
         e.problema.forEach(e => {
-          select3.options[select3.options.length] = new Option(e.valor, e.id) 
-          console.log(e.valor)
+        
+          select3.options[select3.options.length] = new Option(e.valor, e.id,) 
+          console.log(e.id)
           });
    
 });// Essa parte está buscando dados do banco e criando selects no html
+
+
 
 
 
